68bdf8b6fe026e43eb8315bb7978792b5cb4e348
balag3 <carbalage@gmail.com>
2016-08-09 12:44:26 +0200

    Adds comments.Deletes test lines.

--- device.py
+++ device.py
@@ -1,5 +1,4 @@
 import evdev
-import threading
 from evdev import categorize, ecodes
 
 
@@ -7,7 +6,8 @@ class Device():
     name = 'Sycreader RFID Technology Co., Ltd SYC ID&IC USB Reader'
 
     @classmethod
-    def list(cls,show_all=False):
+    def list(cls, show_all=False):
+        # list the available devices
         devices = [evdev.InputDevice(fn) for fn in evdev.list_devices()]
         if show_all:
             for device in devices:
@@ -16,31 +16,37 @@ class Device():
 
     @classmethod
     def connect(cls):
-        device = [dev for dev in cls.list() if cls.name in dev.name][0]
-        device = evdev.InputDevice(device.fn)
-        return device
-
+        # connect to device if available
+        try:
+            device = [dev for dev in cls.list() if cls.name in dev.name][0]
+            device = evdev.InputDevice(device.fn)
+            return device
+        except IndexError:
+            print("Device not found.\n - Check if it is properly connected. \n - Check permission of /dev/input/ (see README.md)")
+            exit()
 
     @classmethod
     def run(cls):
         device = cls.connect()
         container = []
         try:
+            # bind the device to the script
             device.grab()
             print("RFID scanner is ready....")
             print("Press Control + c to quit.")
             for event in device.read_loop():
-                if len(container) == 10:
-                    tag = "".join(i for i in container)
-                    print(tag)
-                    container = []
-                if event.type == ecodes.EV_KEY and event.value == 1 and event.code <= 11:
-                    digit = evdev.ecodes.KEY[event.code].strip("KEY_")
-                    container.append(digit)
+                    # enter into an endeless read-loop
+                    if event.type == ecodes.EV_KEY and event.value == 1 and event.code <= 11:
+                        # filter the input coming from the device and append to our container
+                        digit = evdev.ecodes.KEY[event.code].strip("KEY_")
+                        container.append(digit)
+                    if len(container) == 10:
+                        # create and dump the ten digit rfid tag for further use
+                        tag = "".join(i for i in container)
+                        print(tag)
+                        container = []
 
         except:
+            # catch all exceptions to be able release the device
             device.ungrab()
             print('Quitting.')
-
-
-Device.run()
